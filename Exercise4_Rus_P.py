b103835a7723e71ed4aa3b2e76fa253499e5f630
Russyii <ryosohk@gmail.com>
2020-04-21 13:10:33 +0700

    Create Exercise4_Rus_P.py

---
+++ Exercise4_Rus_P.py
@@ -0,0 +1,9 @@
+FE = 58
+GB = 75.4
+ICS = 67
+CP = 80.3
+print("---YOUR SCORE---")
+print("Foundation English",":",FE)
+print("General Business",":",GB)
+print("Introduction to Computer Systems",":",ICS)
+print("Computer Programming",":",CP)
