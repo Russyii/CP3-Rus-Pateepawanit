9bdd2f0c1514c15817220972b91e458fd858a06a
Russyii <ryosohk@gmail.com>
2020-05-08 12:34:31 +0700

    Create Leture50_Rus_P.py

---
+++ Leture50_Rus_P.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(2,4)
+minusNumber(3,8)
+multiplyNumber(7,9)
+divideNumber(6,2)
